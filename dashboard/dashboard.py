51aabec5628dc3b160b1e260f351e629ff7ef93c
mateus-henriquee <mateush.leccese@gmail.com>
2025-08-19 11:27:12 -0300

    update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# --- Configuração da Página ---
+# - Configuração da Página -
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
 st.set_page_config(
     page_title="Dashboard de Salários na Área de Dados",
